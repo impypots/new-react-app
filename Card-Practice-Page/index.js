4a84c65b312ed43d88d2165a938072fcccbac13a
impypots <72578093+impypots@users.noreply.github.com>
2021-04-21 17:41:22 -0600

    essentially finished with this little project now!

--- Card-Practice-Page/index.js
+++ Card-Practice-Page/index.js
@@ -1,6 +1,38 @@
 let card1 = document.querySelector("#graycard1");
-let infotab1 = document.querySelector(".infotab")
+let card2 = document.querySelector("#graycard2");
+let card3 = document.querySelector("#graycard3");
+let infotab = document.querySelectorAll(".infotab");
+let infotableft = document.querySelector(".infotableft")
 
 card1.addEventListener("click", () => {
-    infotab1.style.display === 'none' ? infotab1.style.display = 'block' : infotab1.style.display = 'none';
+    if (infotab[0].style.transform === 'scaleX(0)') {
+        infotab[0].style.transform = 'scaleX(1)';
+        infotab[1].style.transform = 'scaleX(0)';
+        infotableft.style.transform = 'scaleX(0)';
+    } else {
+        infotab[0].style.transform = 'scaleX(0)';
+    }
 }, false)
+
+card2.addEventListener("click", () => {
+    if (infotab[1].style.transform === 'scaleX(0)') {
+        infotab[1].style.transform = 'scaleX(1)';
+        infotab[0].style.transform = 'scaleX(0)';
+        infotableft.style.transform = 'scaleX(0)';
+    } else {
+        infotab[1].style.transform = 'scaleX(0)';
+    }
+}, false)
+
+card3.addEventListener("click", () => {
+    if (infotableft.style.transform === 'scaleX(0)') {
+        infotableft.style.transform = 'scaleX(1)';
+        infotab[0].style.transform = 'scaleX(0)';
+        infotab[1].style.transform = 'scaleX(0)';
+    } else {
+        infotableft.style.transform = 'scaleX(0)';
+    }
+}, false)
+
+
+// infotab[0].style.transform === 'scaleX(0)' ? infotab[0].style.transform = 'scaleX(1)': infotab[0].style.transform = 'scaleX(0)';
